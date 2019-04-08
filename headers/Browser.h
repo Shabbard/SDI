470916cf594d7e81e5acf7bfe4a2dd88bb908f24
Adam Sheppard <t7.shab@gmail.com>
2019-04-08 17:31:41 +0100

    Browser can add and display

--- headers/Browser.h
+++ headers/Browser.h
@@ -2,10 +2,14 @@
 
 class Browser
 {
+    public:
     Film Data;
     Browser* next;
     Browser* previous;
     Browser* head;
     Browser* tail;
+
+    void insert(Film new_data);
+    void display();
 
 }
