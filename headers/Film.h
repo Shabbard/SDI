f087e2c050aaf38991aa92532edc7db6da11d783
Adam Sheppard <t7.shab@gmail.com>
2019-04-08 15:09:14 +0100

    Added basic film object

---
+++ headers/Film.h
@@ -0,0 +1,9 @@
+#include <string>
+
+class Film
+{
+    private:
+    std::string Title, Summary, Genre, ReleaseDate, FilmingLocations, Langauges, KeyWords;
+    int Runtime;
+    double WeeklyTicketSale;
+}
