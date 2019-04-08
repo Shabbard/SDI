29341da508f992a35ff3276a354ad147a71758d0
Adam Sheppard <t7.shab@gmail.com>
2019-04-08 14:40:44 +0100

    Updated Film.h with declarations for functions to return attributes

--- headers/Film.h
+++ headers/Film.h
@@ -2,8 +2,16 @@
 
 class Film
 {
-    private:
-    std::string Title, Summary, Genre, ReleaseDate, FilmingLocations, Langauges, KeyWords;
+  private:
+    std::string Title, Summary, ReleaseDate;
+    std::vector<std::string> Genre, FilmingLocations, Langauges, KeyWords;
     int Runtime;
     double WeeklyTicketSale;
+
+  public:
+    Film();
+    std::string GetTitle(), GetSummary(), GetReleaseDate();
+    std::vector<std::string> GetGenre(), GetFilmingLocations(), GetLanguages(), GetKeyWords();
+    int GetRuntime();
+    double GetWeeklyTicketSales();
 }
