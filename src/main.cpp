298e6729553245335d3ff3075b5e7ca73b1a4056
Adam Sheppard <t7.shab@gmail.com>
2019-04-08 17:46:47 +0100

    Added Alex's work

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 #include "Film.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <regex>
 
 int main()
 {
-    
+
+	std::ifstream infile;
+	infile.open("Database_Data.txt");
+
+	std::string str;
+
+	while (std::getline(infile, str))
+	{
+		std::regex Film_Test("<Film>");
+
+		if (!regex_search(str, Film_Test))
+		{
+			return false;
+		}
+
+		std::regex Film_Test("<Title>");
+		std::regex Film_Test("(? <= >).*$");
+
+		if (regex_search(str, Film_Test)) 
+		{
+						
+			return false;
+		}
+	}
+
+	return 0;
 }
+
+
+
