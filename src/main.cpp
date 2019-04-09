467c9c6625fa58e6bceed7bdc5709b77d5e03447
Alexander Cain <cainalex33@gmail.com>
2019-04-09 16:18:26 +0100

    Functioning txt cutter

--- src/main.cpp
+++ src/main.cpp
@@ -12,26 +12,35 @@ int main()
 
 	std::string str;
 
+	std::string values[10] = {"ID","Title", "Summary", "Genre", "Release Date", "Filming Locations", "Language", "Runtime", "Keywords", "Weekly Ticket Sales"};
+	std::vector<std::string> LineData;
+
 	while (std::getline(infile, str))
 	{
-		std::regex Film_Test("<Film>");
+		std::regex data("([^,]+)|[\s]"); // finds all of the data values between the commas and includes spaces for empty data values
+  		std::smatch data_match;
 
-		if (!regex_search(str, Film_Test))
-		{
-			return false;
-		}
-
-        std::regex Title_Test("<Title>");
-        std::regex Data("(? <= >).*$");
-
-		if (regex_search(str, Film_Test)) 
-		{
-						
-			return false;
-		}
+  		if (std::regex_search(str, data_match, data)) // if there are data values to enter
+  		{
+    		for (std::sregex_iterator i = std::sregex_iterator(str.begin(), str.end(), data); i != std::sregex_iterator(); ++i)
+    		{
+      			data_match = *i;
+      			DecomposedSentence.second.push_back(data_match.str()); // loops through the values between commas (including whitespace) and adds them to a vector
+    		}
+		}	
 	}
 
-	return 0;
+		
+
+
+
+std::regex Title_Test("<Title>");
+std::regex Data(".*> ");
+
+
+	
+
+	
 }
 
 
