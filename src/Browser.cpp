1f7af02739e78f982f83ec27bec74dad32777dd2
Adam Sheppard <t7.shab@gmail.com>
2019-04-09 16:17:56 +0100

    Added a next and previous function to the browser

--- src/Browser.cpp
+++ src/Browser.cpp
@@ -17,7 +17,35 @@ void Browser::insert(int new_data)
    if(tail !=  NULL)
    {
       tail->next = new_node ; 
-   } 
+   }
+   if (tail == NULL)
+   {
+      head = new_node;
+       current = head;
+   }
    tail = new_node; 
+}
 
+void Browser::next()
+{
+   if (!current->next == NULL)
+   {
+      current = current->next;
+   }
+   else
+   {
+      std::cout << "This is the last item in the list";
+   }
+}
+
+void Browser::previous()
+{
+   if (!current->previous == NULL)
+   {
+      current = current->previous;
+   }
+   else
+   {
+      std::cout << "This is the first item in the list";
+   }
 }
