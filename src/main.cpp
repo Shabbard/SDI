e90d42f2f352c4210d10304cc4f63e0ce0a63906
Alexander Cain <cainalex33@gmail.com>
2019-04-11 18:13:58 +0100

    Updated file name

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,21 @@
 
 void LoadFile(Browser* browser);
 void UpdateFile(Browser* browser);
-void WriteToFile(Browser* browser, std::ofstream& file);
 
 int main()
 {
     Browser* browser = new Browser;
 
+
+	// for (int i = 0; i < 6; i++)
+    // {
+    //     browser->insert_tail(i);
+	// }
+
 	std::string strinput = "";
+    //int input = 0;
+
+	LoadFile(browser);
 
 	do 
 	{
@@ -34,15 +42,11 @@ int main()
 		{
 			browser->delete_current();
 		}
-		if (strinput == "load")
-		{
-			LoadFile(browser);
-		}
-		if (strinput == "save")
-		{
-			UpdateFile(browser);
-			browser->setHead();
-		}
+		// if (striput == "")
+		// {
+
+		// }
+
 		// if (strinput == "insert")
 		// {
 		// 	std::cout << "Please insert a number: ";
@@ -65,21 +69,22 @@ int main()
 	
 }
 
+
 void LoadFile(Browser* browser)
 {
 	std::ifstream infile;
-    infile.open("../data/test.txt");
+    infile.open("../Film_Info.txt");
 
 	Film film;
 
 	std::string str;
 
-	//std::string values[10] = {"ID","Title", "Summary", "Genre", "Release Date", "Filming Locations", "Language", "Runtime", "Keywords", "Weekly Ticket Sales"};
+	std::string values[10] = {"ID","Title", "Summary", "Genre", "Release Date", "Filming Locations", "Language", "Runtime", "Keywords", "Weekly Ticket Sales"};
 	std::vector<std::string> LineData;
 	
 	while (std::getline(infile, str))
 	{
-		std::regex data("([^,]+)"); // finds all of the data values between the commas and includes spaces for empty data values
+		std::regex data("([^,]+)|[\s]"); // finds all of the data values between the commas and includes spaces for empty data values
   		std::smatch data_match;
 
   		if (std::regex_search(str, data_match, data)) // if there are data values to enter
@@ -91,46 +96,55 @@ void LoadFile(Browser* browser)
     		}
 		}	
 
-		if(LineData.at(0)== "ID") // find a better way of doing this
+		if(LineData.at(0)== "ID")
 		{
 			film.ID = std::stoi(LineData.at(1));
 		}
+
 		if(LineData.at(0)== "Title")
 		{
 			film.Title = LineData.at(1);
 		}
+
 		if(LineData.at(0)== "Summary")
 		{
 			film.Summary = LineData.at(1);
 		}
+
 		if(LineData.at(0)== "Genre")
 		{
 			LineData.erase(LineData.begin());
 			film.Genre = LineData;
 		}
+
 		if(LineData.at(0)== "Release_Date")
 		{
 			film.ReleaseDate = LineData.at(1);
 		}
+
 		if(LineData.at(0)== "Filming_Loc")
 		{
 			LineData.erase(LineData.begin());
 			film.Filming_Locations = LineData;
 		}
+
 		if(LineData.at(0)== "Language")
 		{
 			LineData.erase(LineData.begin());
 			film.Languages = LineData;
 		}
+
 		if(LineData.at(0)== "Runtime")
 		{
 			film.Runtime = std::stoi(LineData.at(1));
 		}
+
 		if(LineData.at(0)== "Keywords")
 		{
 			LineData.erase(LineData.begin());
 			film.KeyWords = LineData;
 		}
+
 		if(LineData.at(0)== "Weekly Ticket Sales")
 		{
 			film.WeeklyTicketSale = std::stoi(LineData.at(1));
@@ -140,58 +154,20 @@ void LoadFile(Browser* browser)
 	}
 } 
 
+
+
 void UpdateFile(Browser* browser){
 
 	browser->setHead();
+	
+	std::ofstream newfile("../test.txt");
 
-	std::string filePath = "../data/test.txt";
-	std::ofstream newfile(filePath);
+	std::string str;
 
-	while(browser->current != browser->tail)
-	{		
-		WriteToFile(browser, newfile);
-
-		if(browser->current->next == browser->tail) // please find a better way of doing this
-		{
-			browser->nextNode();
-			WriteToFile(browser, newfile);
-		}
-
-		browser->nextNode();
-	}
-}
-
-void WriteToFile(Browser* browser, std::ofstream& file)
-{
-	file << "ID," << browser->current->data.ID << std::endl;
-	file << "Status," << browser->current->data.Status << std::endl;
-	file << "Title," << browser->current->data.Title << std::endl;
-	file << "Summary," << browser->current->data.Summary << std::endl;
-	file << "Genre,";
-	for(std::vector<std::string>::iterator it = browser->current->data.Genre.begin(); it != browser->current->data.Genre.end(); it++)
+	while(browser->current->next != NULL)
 	{
-		file << *it << ",";
-	}
-	file << std::endl;
-	file << "Release_Date," << browser->current->data.ReleaseDate << std::endl;
-	file << "Filming_Loc,";
-	for(std::vector<std::string>::iterator it = browser->current->data.Filming_Locations.begin(); it != browser->current->data.Filming_Locations.end(); it++)
-	{
-		file << *it << ",";
-	}
-	file << std::endl;
-	file << "Language,";
-	for(std::vector<std::string>::iterator it = browser->current->data.Languages.begin(); it != browser->current->data.Languages.end(); it++)
-	{
-		file << *it << ",";
-	}
-	file << std::endl;
-	file << "Runtime," << browser->current->data.Runtime << std::endl;
-	file << "Keywords,";
-	for(std::vector<std::string>::iterator it = browser->current->data.KeyWords.begin(); it != browser->current->data.KeyWords.end(); it++)
-	{
-		file << *it << ",";
-	}
-	file << std::endl;
-	file << "Weekly Ticket Sales," << browser->current->data.WeeklyTicketSale << std::endl;
+		newfile << "ID," << browser->current->data.ID << std::endl;
+	} 
+
+
 }
