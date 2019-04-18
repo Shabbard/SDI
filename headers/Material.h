85a22441651842c6500d2beacbb8395020b2063c
Adam Sheppard <t7.shab@gmail.com>
2019-04-18 19:42:06 +0100

    Refactored Material.h

--- headers/Material.h
+++ headers/Material.h
@@ -1,9 +1,7 @@
 #pragma once
 #ifndef MATERIAL_H
 #define MATERIAL_H
-//#ifndef FILM_H
 #include "Film.h"
-//#endif
 #include <string>
 #include <vector>
 #include <memory>
@@ -11,68 +9,80 @@
 class Material
 {
     public:
-        virtual ~Material() {}
         int ID = 0, Runtime = 0;
         std::string Title, VideoFormat, AudioFormat, FrameAspect, Packaging, Description, Type; 
         double RetailPrice = 0;
-        std::vector<std::string> Languages, Subtitles;
-        std::vector<Material*> DVDs;
-        std::pair<Film, Film> DVD;
-        Film film;
+        virtual void SetLanguages(std::vector<std::string>);
+        virtual void SetSubtitiles(std::vector<std::string>);
+        virtual void SetDVDVector(Material*);
+        virtual void SetDoubleDVD(Film, Film);
+        virtual void SetFilm(Film);
 };
 
 class VHS : public Material
-{
+{   
+    private:
+        Film film;
+        std::string Languages, Subtitles;
     public:
      VHS()
      {
          Type = "VHS";
-         std::string Languages, Subtitles;
      }
+     void SetLanguages(std::vector<std::string> input) override {Languages = input.at(0);}
+     void SetSubtitiles(std::vector<std::string> input) override {Subtitles = input.at(0);}
+     void SetFilm(Film in) override {film = in;} 
 };
 
 class DVD : public Material
-{  
+{   
+    private:
+        Film film;
+        std::vector<std::string> Languages, Subtitles;
     public:
         DVD()
         {
             Type = "DVD";
             Packaging = "Plastic Box";
         }
+        void SetFilm(Film in) override {film = in;} 
 };
-
-// class SingleSidedDVD : public DVD
-// {
-//     public:
-        
-// };
 
 class DoubleSidedDVD : public Material
 {
+    private:
+        std::pair<Film, Film> DVD;
+        std::vector<std::string> Languages, Subtitles;
     public:   
      DoubleSidedDVD(){Type = "DoubleSidedDVD";}
+     virtual void SetDoubleDVD(Film, Film);
 };
 
 class BluRay : public DVD
 {
+    private:
+        Film film;
+        std::vector<std::string> Languages, Subtitles;
     public:
         BluRay()
         {
             Type = "BluRay";
             Packaging = "Small Plastic Box";
         }
-        
+        void SetFilm(Film in) override {film = in;}        
 };
 
 class ComboBox : public Material
 {
+    private:
+        std::vector<Material*> DVDs;
+        std::vector<std::string> Languages, Subtitles;
     public:
         ComboBox()
         {
             Type = "ComboBox";
             Packaging = "Cardboard Box";
         }
-
+        void SetDVDVector(Material* in) override {DVDs.push_back(in);}
 };
-
 #endif
